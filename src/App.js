7d5b73519a312c9a5e78ecb1f44bc07ab1377209
KK <yongkang.yan@shopee.com>
2019-08-13 16:05:24 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,93 @@
 import React from 'react';
-import DoubleClickExample from './DoubleClickExample'
+// import DoubleClickExample from './DoubleClickExample'
+
+const useCancellablePromises = () => {
+  const pendingPromises = React.useRef([]);
+
+  const appendPendingPromise = promise =>
+    pendingPromises.current = [...pendingPromises.current, promise];
+
+  const removePendingPromise = promise =>
+    pendingPromises.current = pendingPromises.current.filter(p => p !== promise);
+
+  const clearPendingPromises = () => pendingPromises.current.map(p => p.cancel());
+
+  const api = {
+    appendPendingPromise,
+    removePendingPromise,
+    clearPendingPromises,
+  };
+
+  return api;
+};
+
+const cancellablePromise = promise => {
+  let isCanceled = false;
+
+  const wrappedPromise = new Promise((resolve, reject) => {
+    promise.then(
+      value => (isCanceled ? reject({ isCanceled, value }) : resolve(value)),
+      error => reject({ isCanceled, error }),
+    );
+  });
+
+  return {
+    promise: wrappedPromise,
+    cancel: () => (isCanceled = true),
+  };
+};
+
+export const noop = () => {};
+
+export const delay = n => new Promise(resolve => setTimeout(resolve, n));
+
+const useClickPreventionOnDoubleClick = (onClick, onDoubleClick) => {
+  const api = useCancellablePromises();
+
+  const handleClick = () => {
+    api.clearPendingPromises();
+    const waitForClick = cancellablePromise(delay(300));
+    api.appendPendingPromise(waitForClick);
+
+    return waitForClick.promise
+      .then(() => {
+        api.removePendingPromise(waitForClick);
+        onClick();
+      })
+      .catch(errorInfo => {
+        api.removePendingPromise(waitForClick);
+        if (!errorInfo.isCanceled) {
+          throw errorInfo.error;
+        }
+      });
+  };
+
+  const handleDoubleClick = () => {
+    api.clearPendingPromises();
+    onDoubleClick();
+  };
+
+  return [handleClick, handleDoubleClick];
+};
+
+
+
+const ClickableBox = ({ onClick, onDoubleClick }) => {
+  const [handleClick, handleDoubleClick] = useClickPreventionOnDoubleClick(onClick, onDoubleClick);
+
+  return (
+    <button onClick={handleClick} onDoubleClick={handleDoubleClick}>
+      Click or double click
+    </button>
+  );
+};
+
+
+const DoubleClickExample = () => (
+  <ClickableBox
+    onClick={() => console.log("on click")} 
+    onDoubleClick={() => console.log("on double click")}/>
+);
 
 function App() {
   return (
